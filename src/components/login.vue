37bba492e6af25617c91d817640dcb9169484804
huajuan1002 <2247177088@qq.com>
2021-06-03 16:27:38 +0800

    commit

---
+++ src/components/login.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+<el-button type="text" @click="dialogFormVisible = true">打开嵌套表单的 Dialog</el-button>
+
+<el-dialog title="登录" :visible.sync="dialogFormVisible">
+  <el-form :model="form">
+    <el-form-item label="用户名称" :label-width="formLabelWidth">
+      <el-input v-model="form.name" autocomplete="off"></el-input>
+    </el-form-item>
+     <el-form-item label="用户密码" :label-width="formLabelWidth">
+      <el-input v-model="form.name" autocomplete="off"></el-input>
+    </el-form-item>
+  </el-form>
+  <div slot="footer" class="dialog-footer">
+    <el-button @click="dialogFormVisible = false">登录</el-button>
+    <el-button type="primary" @click="dialogFormVisible = false">确 定</el-button>
+  </div>
+</el-dialog>
+  </div>
+</template>
+<script>
+export default ({
+  data () {
+    return {
+      dialogTableVisible: false,
+      dialogFormVisible: true,
+      formLabelWidth: '120px'
+    }
+  }
+})
+</script>
+<style scoped>
+
+</style>
--- src/router.js
+++ src/router.js
@@ -1,23 +1,17 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/Home.vue'
-
+import Login from '/components/login.vue'
 Vue.use(Router)
 
 export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: Home
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
-    }
-  ]
+     {
+       path:'/',
+       redirect:'/login'
+      //  输入/也会重定向到login页面
+     },
+     {
+       path:'/login',
+       name:'login',
+       component:login
+     }
 })
